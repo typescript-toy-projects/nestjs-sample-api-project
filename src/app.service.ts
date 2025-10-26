d8fcf71bae2fb29128130962f00dd810c122f892
chlee021690 <chlee0216@alumni.upenn.edu>
2025-10-26 19:28:44 +0900

    added one more API to test parameter passing

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getIntroductionWithName(name: string): string {
+    return 'Hello! My name is ' + name;
+  }
 }
